68307ea7a7f4e4bf98244bd2e504930b74afae08
ClaraTschamon <clara.tschamon@students.fhv.at>
2023-05-27 17:00:53 +0200

    Some diagrams

--- Kennzahlen/women.R
+++ Kennzahlen/women.R
@@ -2,26 +2,70 @@
 
 rnorm(Womens_Boston_Marathon_Winners$`Distance (KM)`)
 
-summary(rnorm(Womens_Boston_Marathon_Winners$`Distance (KM)`))
 
-
-# Remove NA fields
+# mean ist 42.2km
 meanDistance <- mean(Womens_Boston_Marathon_Winners$`Distance (KM)`, na.rm=TRUE)
+
+# boxplot zeigt, dass es keine abweichungen gibt. in jedem jahr wurden 42.2 km gerannt
+boxplot(Womens_Boston_Marathon_Winners$`Distance (KM)`)
 
 ##############################################################
 
-library(chron)
+parsed_times <- strptime(Womens_Boston_Marathon_Winners$Time, format="%H:%M:%S")
 
-mean_time_formatted <- format(Womens_Boston_Marathon_Winners$Time, format = "%H:%M:%S")
+numeric_times_in_seconds <- as.numeric(parsed_times)
 
-# Print the mean time
-print(mean_time_formatted)
+mean_time_in_seconds <- mean(Womens_Boston_Marathon_Winners$Time, na.rm = TRUE)
+cat(mean_time_in_seconds)
 
-# Convert character strings to time objects
-time_objects <- as.POSIXct(mean_time_formatted, format = "%H:%M:%S")
+formatted_mean_time <- mean(parsed_times, na.rm = TRUE)
 
-# Calculate the mean time
-mean_time <- mean(time_objects)
+# Create a boxplot of the formatted times
+boxplot(numeric_times_in_seconds, 
+        main = "Boston Marathon Winners", 
+        xlab = "Time (hh:mm:ss)", 
+        col = "lightblue",
+        horizontal = TRUE)
+
+# Streudiagramm: Entfernung
+plot(Womens_Boston_Marathon_Winners$`Distance (KM)`, )
+
+# Streudiagramm: Zeit und Entfernung
+plot(numeric_times_in_seconds, Womens_Boston_Marathon_Winners$`Distance (KM)`)
 
 
-boxplot(sapply(strptime(all_data$Time, format = "%H:%M:%S"), function(x) as.numeric(format(x, "%M"))), type=5, col=c("red"))
+## zahl der gewinner pro land
+# Load required libraries
+install.packages("ggplot2")
+
+library(ggplot2)
+
+# Erstelle ein data.frame mit den Länderzählungen
+country_counts <- table(Womens_Boston_Marathon_Winners$Country)
+
+df <- data.frame(Country = names(country_counts), Count = as.numeric(country_counts))
+
+# Erstelle das Balkendiagramm
+ggplot(df, aes(x = Country, y = Count)) +
+  geom_bar(stat = "identity") +
+  xlab("Land") +
+  ylab("Häufigkeit") +
+  ggtitle("Häufigkeit der Länder der Gewinnerinnen") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+
+# Erstelle ein data.frame mit den Jahreszahlen und den Geschwindigkeiten
+df <- data.frame(Jahr = Womens_Boston_Marathon_Winners$Year, 
+                 Geschwindigkeit = numeric_times_in_seconds)
+
+# Erstelle das Liniendiagramm
+ggplot(df, aes(x = Jahr, y = Geschwindigkeit)) +
+  geom_line() +
+  xlab("Jahr") +
+  ylab("Geschwindigkeit") +
+  ggtitle("Geschwindigkeit im Vergleich zur Jahreszahl") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+
+
+
