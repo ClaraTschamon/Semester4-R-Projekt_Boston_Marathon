69bf86b08609982bac5bc977c85a1105242434d3
Hilal <hoe6831@students.fhv.at>
2023-06-11 13:55:25 +0200

    fetig

--- Kennzahlen/men.R
+++ Kennzahlen/men.R
@@ -14,10 +14,11 @@ meanDistanceKm <- mean(Mens_Boston_Marathon_Winners$Distance..KM., na.rm = TRUE)
 #Boxplot für die Distanz
 boxplot(Mens_Boston_Marathon_Winners$Distance..KM., main = "Boxplot für die Distanz", horizontal = TRUE)
 #macht ned würkli sinn weil man anzahl der läufer nicht sieht
-# Erstelle ein data.frame mit den Länderzählungen
+# Erstelle ein data.frame mit den Distanzzählungen
 distance_counts <- table(Mens_Boston_Marathon_Winners$Distance..KM.)
 
 df <- data.frame(Distance = names(distance_counts), Count = as.numeric(distance_counts))
+df
 
 # Erstelle das Balkendiagramm
 ggplot(df, aes(x = Distance, y = Count)) +
@@ -42,24 +43,28 @@ numeric_times_in_minutes <- numeric_times_in_seconds / 60
 
 
 
-#Boxplot für die Durchschnittszeit
-# Geschwindigkeiten von Männern
-men_times <- format(as.POSIXct(strptime(Mens_Boston_Marathon_Winners$Time, format="%H:%M:%S")), 
-                    format = "%H:%M:%S")
-men_times_hms <- as_hms(men_times) # Umwandlung in Stunden-Minuten-Sekunden
-men_times_in_seconds <- lubridate::seconds(men_times_hms)
-men_times_in_minutes <- as.numeric(men_times_in_seconds / 60)
-men_speeds <- men_times_in_minutes / Mens_Boston_Marathon_Winners$Distance..KM.
 
-# Boxplot für men_speeds erstellen
-boxplot(men_speeds, main = "Boxplot für Geschwindigkeiten der Männer", xlab = "Geschwindigkeit (Minuten pro Kilometer)", horizontal = TRUE)
 
 
 
 
 #DurchschnittsGeschwindigkeit der Männer - 3 min 24 sek
 meanSpeed <- meanTime/meanDistanceKm
+meanSpeed
 
+
+#Boxplot für die Durchschnittgeschwindigkeit
+# Geschwindigkeiten von den einzelnen Männern
+men_times <- format(as.POSIXct(strptime(Mens_Boston_Marathon_Winners$Time, format="%H:%M:%S")), 
+                    format = "%H:%M:%S")
+men_times_hms <- as_hms(men_times) # Umwandlung in Stunden-Minuten-Sekunden
+men_times_in_seconds <- lubridate::seconds(men_times_hms)
+men_times_in_minutes <- as.numeric(men_times_in_seconds / 60)
+men_speeds <- men_times_in_minutes / Mens_Boston_Marathon_Winners$Distance..KM.
+men_speeds
+
+# Boxplot für men_speeds erstellen
+boxplot(men_speeds, main = "Boxplot für Geschwindigkeiten der Männer", xlab = "Geschwindigkeit (Minuten pro Kilometer)", horizontal = TRUE)
 
 
 
@@ -100,3 +105,17 @@ df
 
 # Erstelle Streudiagramm
 plot(df$Geschwindigkeit, df$Distanz, xlab = "Geschwindigkeit", ylab = "Distanz", main ="Vergleich zwischen der Geschwindigkeit und der Entfernung")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
